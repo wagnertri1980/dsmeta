e5d7b8699be04901fe0612ae77231b1d389c4d14
Wagner <wagnertri1980@gmail.com>
2022-09-14 18:31:39 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,10 +1,17 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
 //Interface - Local onde serão realizadas as operações no banco de dados.
 public interface SaleRepository extends JpaRepository<Sale, Long>{
-	
+	//Consulta JPQL, buscando os objetos de venda no período informado entre uma data mínima e uma máxima.
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,15 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -16,8 +23,14 @@ public class SaleService {
 	private SaleRepository repository;
 	
 	//Buscar as vendas
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.now(ZoneId.systemDefault()); //Pegando a data atual do sistema.
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate); //Converte o valor da data de String para o formato LocalDate
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate); //Converte o valor da data de String para o formato LocalDate
+		
+		return repository.findSales(min, max, pageable);
 	}
 
 }
